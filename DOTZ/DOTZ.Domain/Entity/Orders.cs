9e635d8d46b7289e6ae2ceb3b1a87cfeb427b588
victorlouzada <vrlouzada@hotmail.com>
2021-08-20 09:11:16 -0300

    Repositório de Pedidos

---
+++ DOTZ/DOTZ.Domain/Entity/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DOTZ.Domain.Entity
+{
+    [Table("Order")]
+    public class Orders
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CostumerId { get; set; }
+        public int ProductId { get; set; }
+        public int AddressId { get; set; }
+        public int OrderStatusId { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ DOTZ/DOTZ.Test/Repository/OrderRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using DOTZ.CrossCutting.IoC;
+using DOTZ.Domain.Contracts.Repository;
+using DOTZ.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DOTZ.Test.Repository
+{
+    public class OrderRepositoryTest
+    {
+
+        private readonly IOrderRepository _orderRepository;
+
+
+        public OrderRepositoryTest()
+        {
+            var ioc = Startup.IoC();
+            _orderRepository = ioc.GetInstance<IOrderRepository>();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void GetList(int costumerId)
+        {
+            var result = _orderRepository.GetList(costumerId);
+
+            Assert.NotEmpty(result);
+        }
+
+
+        [Theory]
+        [InlineData(2, 5, 6, 2)]
+        public void Insert(int costumerId, int productId, int addressId, int orderStatusId)
+        {
+            var order = new Orders
+            {
+                CostumerId = costumerId,
+                ProductId = productId,
+                AddressId = addressId,
+                OrderStatusId = orderStatusId
+            };
+
+            var result = _orderRepository.Insert(order);
+
+            Assert.True(result);
+        }
+
+    }
+}
